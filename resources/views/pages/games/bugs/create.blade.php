06118ff425925112b3b41f7a6869688c42c1b495
MrMimi11 <remi.prat@laposte.net>
2021-06-06 23:50:08 +0200

    [FEA] Added Request for registration and indent the code in create a bug in the view

--- resources/views/pages/games/bugs/create.blade.php
+++ resources/views/pages/games/bugs/create.blade.php
@@ -2,7 +2,8 @@
 @section('content')
     {{ debug(session('errors')) }}
     <h2 class="text-center">Post a bug</h2>
-    <form action="{{ route('games.bugs.store', [$game->slug, $bug->slug]) }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('games.bugs.store', [$game->slug, $bug->slug]) }}" method="post"
+          enctype="multipart/form-data">
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
@@ -36,7 +37,8 @@
 
             @foreach($categories as $category)
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="{{ $category->id }}" title="{{ $category->name }}" value="{{ $category->id }}" name="categories[]">
+                    <input type="checkbox" class="form-check-input" id="{{ $category->id }}"
+                           title="{{ $category->name }}" value="{{ $category->id }}" name="categories[]">
                     <label class="form-check-label" for="{{ $category->id }}">{{ $category->name }}</label>
                 </div>
             @endforeach
